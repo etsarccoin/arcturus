0af4849d777616418c9649a33cc9c9883fab0d8b
Himalaya <etsindiapython>
2019-10-15 06:07:41 +0530

    changes on regitrstion

--- UserApp/MyHelpPackage.py
+++ UserApp/MyHelpPackage.py
@@ -15,26 +15,27 @@ def SendMail(toaddr, message):
     # fromaddr = "arcturuscoin@gmail.com"
     # password = "arc@2019"
     # toaddr = toaddr
-    subject = 'Arcturus'
-    message = 'Subject: {}\n\n{}'.format(subject, message)
+    # subject = 'Arcturus'
+    # message = 'Subject: {}\n\n{}'.format(subject, message)
     
+    # # try:
+    # #     server = smtplib.SMTP('smtp.gmail.com', 587)  ## smtp.mail.yahoo.com
+    # #     server.starttls()
+    # #     server.ehlo()
+    # #     server.login(fromaddr, password)
+    # #     server.sendmail(fromaddr, toaddr, message)
+    # #     server.quit()
+    # # except SMTPException:
+    # #     print("Mail id is Invalid !!")
     # try:
-    #     server = smtplib.SMTP('smtp.gmail.com', 587)  ## smtp.mail.yahoo.com
-    #     server.starttls()
-    #     server.ehlo()
-    #     server.login(fromaddr, password)
-    #     server.sendmail(fromaddr, toaddr, message)
-    #     server.quit()
-    # except SMTPException:
-    #     print("Mail id is Invalid !!")
-    try:
-        s = smtplib.SMTP('smtp.gmail.com', 587) 
-        s.starttls() 
-        s.login("arcturuscoin@gmail.com", "arc@2019")  
-        s.sendmail("arcturuscoin@gmail.com", toaddr, message) 
-        s.quit()
-    except Exception as e:
-        print("HI I AM FROM MAIL SEND ",e)
+    #     s = smtplib.SMTP('smtp.gmail.com', 587) 
+    #     s.starttls() 
+    #     s.login("arcturuscoin@gmail.com", "arc@2019")  
+    #     s.sendmail("arcturuscoin@gmail.com", toaddr, message) 
+    #     s.quit()
+    # except Exception as e:
+    #     print("HI I AM FROM MAIL SEND ",e)
+    print("hello")
 
 
 def SendMailWithSubject(toaddr, subject, message):
