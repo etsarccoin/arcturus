7d125a670a462378fb25b63ad5bdaea14a72f509
kuntal-python <kuntalbabu.s@gmail.com>
2019-10-15 19:05:36 +0530

    admin.py changed

--- UserApp/admin.py
+++ UserApp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import EmailVerifyCodes, ForgetPasswordTable, \
     CoinRequest, CoinPrice, SubscriptionTable, UserAccountCoin, UserCredintials,AdminWhitePaper
-from .models import UsersD as UsersDetail
+from .models import UsersD
 
-admin.site.register(UsersDetail)
+admin.site.register(UsersD)
 admin.site.register(EmailVerifyCodes)
 admin.site.register(ForgetPasswordTable)
 admin.site.register(CoinRequest)
